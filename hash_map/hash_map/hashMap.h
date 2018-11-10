bb089277d2f91146d1c1e732adad52d270898eb6
talsim <tals0606@gmail.com>
2018-11-10 20:36:20 +0200

    finished insert function and changed display to cout operator wihout fully implementation

--- hash_map/hash_map/hashMap.h
+++ hash_map/hash_map/hashMap.h
@@ -2,7 +2,7 @@
 #include <string>
 #include <array>
 
-#define MAX_SIZE 64
+#define MAX_SIZE 32
 
 std::string to_string(const std::string& val)
 {
@@ -21,13 +21,13 @@ public:
 	{
 	}
 	void insert(K key, V value);
-	void display();
+	bool contains(const K key);
+	template <typename K, typename V> friend std::ostream& operator<<(std::ostream& os, const HashMap<K, V>& dt);
 
 private:
 	std::array<std::vector<std::pair<K, V>>, MAX_SIZE> data;
 	size_t curr_size;
 	int hashCode(K);
-	bool is_last_pair();
 };
 
 template <typename K, typename V>
@@ -43,30 +43,58 @@ int HashMap<K, V>::hashCode(K key)
 	return index;
 }
 // TODO: implement the best hashCode to avoid chaning
-
-template<typename K, typename V>
-bool HashMap<K, V>::is_last_pair()
+template <typename K, typename V>
+std::ostream& operator<<(std::ostream& os, const HashMap<K, V>& dt)
 {
-	return false;
+	std::string storage_str = "";
+	os << "{";
+	for (int i = 0; i < MAX_SIZE; i++)
+	{
+		for (int j = 0; j < dt[i].size(); j++) // for on pairs
+		{
+			storage_str += to_string(dt[i][j].first);
+			storage_str += ":";
+			storage_str += to_string(dt[i][j].second);
+			storage_str += ",";
+		}
+	}
+	os << "}";
+	return os;
 }
 
 template<typename K, typename V>
 void HashMap<K, V>::insert(const K key, const V value)
 {
+	if (contains(key))
+	{
+		std::cout << "Error: key " << key << " already exists!" << std::endl;
+		exit(0);
+	}
 	data[hashCode(key)].push_back(std::make_pair(key, value));
 	curr_size++;
 }
 
-// python style printing
 template<typename K, typename V>
-void HashMap<K, V>::display()
+bool HashMap<K, V>::contains(const K key)
 {
-	std::cout << "{";
-	for (int i = 0; i <	MAX_SIZE; i++)
+	for (int i = 0; i < MAX_SIZE; i++)
 	{
-		for (int j = 0; j < data[i].size(); j++) // for on the pairs
-			std::cout << data[i][j].first << ":" << data[i][j].second << ", ";
-			if(is_last_pair())
-				std::cout << "}" << std::endl;
+		for (int j = 0; j < data[i].size(); j++)
+			if (data[i][j].first == key)
+				return true;
 	}
+	return false;
 }
+
+// python style printing
+//void HashMap<K, V>::display()
+//{
+//	std::cout << "{";
+//	for (int i = 0; i <	MAX_SIZE; i++)
+//	{
+//		for (int j = 0; j < data[i].size(); j++) // for on the pairs
+//			std::cout << data[i][j].first << ":" << data[i][j].second << ", ";
+//			if()
+//				std::cout << "}" << std::endl;
+//	}
+//}
