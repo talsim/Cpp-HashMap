fafeec2925f337aa55e5cd05022cb015bda79175
Tal <Tal@TAL-COMPUTER>
2020-03-15 14:00:29 +0200

    empty main

--- HashMap/HashMap/main.cpp
+++ HashMap/HashMap/main.cpp
@@ -1,17 +1,8 @@
 #include "HashMap.h"
-#include <iostream>
-#include <string>
-using namespace std;
 
 int main()
 {
-	HashMap<string, int> obj;
-	obj.insert("tal", 1000);
-	obj.insert("eli", 900);
-	obj.insert("mama", 800);
-	obj.insert("hello", 700);
-	cout << obj;
-	obj.get("lol");
-	return 0;
+
 }
+
 
